a99661dc83f219c43af51096d869b97b9a92b347
drd-2rcl <drd.rclrd@gmail.com>
2019-06-16 13:10:30 -0300

    was added component header in app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/header'
+
 const App = () => (
   <BrowserRouter>
+    <Header />
     <Routes />
   </BrowserRouter>
 )
